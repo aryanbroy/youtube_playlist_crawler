5c47f8e44fcfc0479afccddb654a285893731f5f
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-09-25 17:35:30 +0530

    get video titles of a particular playlist

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-// You don't need to import RequestQueue anymore
-import { CheerioCrawler } from "crawlee";
+// Instead of CheerioCrawler let's use Playwright
+// to be able to render JavaScript.
+import { PlaywrightCrawler } from "crawlee";
 
-const crawler = new CheerioCrawler({
-  async requestHandler({ $, request }) {
-    const title = $("title").text();
-    console.log(`The title of "${request.url}" is: ${title}.`);
+const crawler = new PlaywrightCrawler({
+  requestHandler: async ({ page }) => {
+    await page.waitForSelector("a#video-title.yt-simple-endpoint");
+
+    const videoTitle = await page.$$eval(
+      "a#video-title.yt-simple-endpoint",
+      (els) => {
+        return els.map((el) => el.textContent);
+      }
+    );
+
+    videoTitle.forEach((text, i) => {
+      console.log(`CATEGORY_${i + 1}: ${text}\n`);
+    });
   },
 });
 
-// Start the crawler with the provided URLs
-await crawler.run(["https://crawlee.dev"]);
+await crawler.run([
+  "https://www.youtube.com/playlist?list=PLRD1Niz0lz1sfeX5UEGzkC3HJGpPOE96b",
+]);
