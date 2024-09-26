7585d4aa44ffa98af95810998f0c295fd1479b48
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-09-26 13:00:55 +0530

    better way to scare playlist data

--- index.js
+++ index.js
@@ -2,77 +2,65 @@ import { PlaywrightCrawler, Dataset } from "crawlee";
 
 const crawler = new PlaywrightCrawler({
   requestHandler: async ({ page }) => {
-    await page.waitForSelector("a#video-title.yt-simple-endpoint");
+    // better way
+    await page.waitForSelector("ytd-playlist-video-renderer");
 
-    const videoTitles = await page.$$eval(
-      "a#video-title.yt-simple-endpoint",
-      (els) => {
-        return els.map((el) => el.title);
+    const videoDetails = await page.$$eval(
+      "ytd-playlist-video-renderer",
+      (videoDiv) => {
+        const videoDetail = videoDiv.map((video) => {
+          const videoTitle = video.querySelector(
+            "div#content > div#container > div#meta > h3 > a#video-title"
+          ).title;
+
+          const videoThumbnail = video.querySelector(
+            "div#content > div#container > ytd-thumbnail#thumbnail > a#thumbnail > yt-image > img"
+          ).src;
+
+          const result = {
+            title: videoTitle,
+            thumbnail: videoThumbnail,
+          };
+
+          return result;
+        });
+        return videoDetail;
       }
     );
+    console.log(videoDetails);
 
-    const thumbnails = await page.$$eval(
-      "ytd-thumbnail#thumbnail > a > yt-image > img.yt-core-image",
-      (images) => {
-        return images.map((image) => image.src);
-      }
-    );
+    // will get the job done
+    // await page.waitForSelector("a#video-title.yt-simple-endpoint");
 
-    const pairedData = videoTitles.map((title, i) => ({
-      title,
-      thumbnail: thumbnails[i] || "N/A",
-    }));
+    // const videoTitles = await page.$$eval(
+    //   "a#video-title.yt-simple-endpoint",
+    //   (els) => {
+    //     return els.map((el) => el.title);
+    //   }
+    // );
 
-    pairedData.forEach(async (item, i) => {
-      // console.log(`Title: ${item.title}, Thumbnail: ${item.thumbnail}`);
-      await Dataset.pushData({
-        title: item.title,
-        thumbnail: item.thumbnail,
-      });
-    });
+    // const thumbnails = await page.$$eval(
+    //   "ytd-thumbnail#thumbnail > a > yt-image > img.yt-core-image",
+    //   (images) => {
+    //     return images.map((image) => image.src);
+    //   }
+    // );
+
+    // const pairedData = videoTitles.map((title, i) => ({
+    //   title,
+    //   thumbnail: thumbnails[i] || "N/A",
+    // }));
+
+    // pairedData.forEach(async (item, i) => {
+    //   // console.log(`Title: ${item.title}, Thumbnail: ${item.thumbnail}`);
+    //   await Dataset.pushData({
+    //     title: item.title,
+    //     thumbnail: item.thumbnail,
+    //   });
+    // });
   },
 });
 
 await crawler.run([
   "https://www.youtube.com/playlist?list=PLRD1Niz0lz1sfeX5UEGzkC3HJGpPOE96b",
 ]);
-
-// below code is not working
-// import { PlaywrightCrawler } from "crawlee";
-
-// const crawler = new PlaywrightCrawler({
-//   requestHandler: async ({ page, request, enqueueLinks }) => {
-//     console.log(`Processing: ${request.url}`);
-//     if (request.label === "VIDEO") {
-//       // await page.waitForSelector("a#video-title.yt-simple-endpoint", {
-//       //   timeout: 10000,
-//       // });
-//       // const title = await page
-//       //   .locator("a#video-title.yt-simple-endpoint")
-//       //   .innerText();
-//       // console.log(`Title: ${title}`);
-//       // console.log("Video found");
-//       const title = await page
-//         .locator(
-//           "h3.style-scope.ytd-playlist-video-renderer > a#video-title.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer"
-//         )
-//         .textContent();
-//       // console.log(await title.innerText());
-//       console.log(title);
-//     } else {
-//       await page.waitForSelector(
-//         "a#video-title.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer"
-//       );
-
-//       await enqueueLinks({
-//         selector:
-//           "a#video-title.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer",
-//         label: "VIDEO",
-//       });
-//     }
-//   },
-// });
-
-// await crawler.run([
-//   "https://www.youtube.com/playlist?list=PLRD1Niz0lz1sfeX5UEGzkC3HJGpPOE96b",
-// ]);
